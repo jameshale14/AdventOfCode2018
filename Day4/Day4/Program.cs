729a8e902f5e26e5cf4c115e3ddef04cde6d1605
James Hale <james.hale.1890@googlemail.com>
2019-03-26 15:44:20 +0000

    Completed Day 4 Part 2

--- Day4/Day4/Program.cs
+++ Day4/Day4/Program.cs
@@ -12,8 +12,11 @@ namespace Day4
             SnoozeCatcher snoozeCatcher = new SnoozeCatcher();
 
             var part1 = snoozeCatcher.GetSnooziestGuard(input);
+            var part2 = snoozeCatcher.GetSnooziestMinuteForOneGuard(input);
 
-            Console.WriteLine($"Part 1: { part1.guardID } x { part1.minute } = { part1.guardID * part1.minute} ");
+            Console.WriteLine($"Part 1: Guard ID { part1.guardID } x minute { part1.minute } = { part1.guardID * part1.minute}");
+
+            Console.WriteLine($"Part 2: Guard ID { part2.guardID } x minute { part2.minute } = { part2.guardID * part2.minute}");
         }
     }
 }
--- Day4/Day4/SnoozeCatcher.cs
+++ Day4/Day4/SnoozeCatcher.cs
@@ -101,5 +101,83 @@ namespace Day4
             return (sleepiestGuard, sleepiestMinute);
         }
 
+        public (int guardID, int minute) GetSnooziestMinuteForOneGuard(string input)
+        {
+            string[] inputArray = input.Split(new string[] { "\n", "\r\n" }, StringSplitOptions.RemoveEmptyEntries);
+
+            SortedDictionary<string, string> timestamps = new SortedDictionary<string, string>();
+            foreach (string line in inputArray)
+            {
+                string[] lineArr = line.Split(']');
+                timestamps.Add(lineArr[0], lineArr[1]);
+            }
+
+            // go through each line of the input, total up the time spent asleep, and the number of times each minute was spent asleep by each guard
+
+            Dictionary<int, Dictionary<int, int>> guards = new Dictionary<int, Dictionary<int, int>>();
+            //guards[guardID][minute] - value = frequency of minute asleep
+
+            int guardId = 0;
+            var guardRegex = new Regex(@"#(\d+) ");
+            var minuteRegex = new Regex(@":(\d\d)");
+
+            int sleepTime = 0;
+            int wakeTime = 0;
+            foreach (var entry in timestamps)
+            {
+                if (entry.Value.Contains("begins"))
+                {
+                    //get the guardID
+                    var match = guardRegex.Match(entry.Value);
+                    guardId = Int32.Parse(match.Groups[1].Value);
+                    guards.TryAdd(guardId, new Dictionary<int, int>());
+
+                }
+                else if (entry.Value.Contains("falls"))
+                {
+                    var minuteMatch = minuteRegex.Match(entry.Key);
+                    sleepTime = Int32.Parse(minuteMatch.Groups[1].Value);
+                }
+                else if (entry.Value.Contains("up"))
+                {
+                    //get the awake time
+                    var minuteMatch = minuteRegex.Match(entry.Key);
+                    wakeTime = Int32.Parse(minuteMatch.Groups[1].Value);
+
+                    // add entries for the guardID for each minute between asleep time and wake up time
+                    for (int i = sleepTime; i < wakeTime; i++)
+                    {
+                        if (guards[guardId].TryGetValue(i, out var val))
+                        {
+                            guards[guardId][i]++;
+                        }
+                        else
+                        {
+                            guards[guardId].Add(i, 1);
+                        }
+                    }
+                }
+            }
+
+            // get the minute that was spent asleep most by the same guard
+            int sleepiestMinute = 0;
+            int minutesSlept = 0;
+            int snoozyGuard = 0;
+            foreach (var guard in guards)
+            {
+                foreach (var minute in guard.Value)
+                {
+                    if (minutesSlept < minute.Value)
+                    {
+                        sleepiestMinute = minute.Key;
+                        minutesSlept = minute.Value;
+                        snoozyGuard = guard.Key;
+                    }
+                }
+            }
+
+            return (snoozyGuard, sleepiestMinute);
+        }
     }
+
 }
